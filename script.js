1846a2ff7bf6fba775803c6f96e3d8b0cb74c916
kietdo04052005-maker <kietdo04052005@gmail.com>
2026-03-30 16:04:49 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Portfolio của Đỗ Bá Kiệt đã sẵn sàng!');
-    // Một câu chào nhỏ khi truy cập
-    setTimeout(() => {
-        alert('Chào mừng bạn đến với không gian của Đỗ Bá Kiệt - Future Network Engineer!');
-    }, 500);
-});
