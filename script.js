95a4d916eff35c981a4cee3d16e6ed9f1ddd9b54
kietdo04052005-maker <kietdo04052005@gmail.com>
2026-03-30 15:48:52 +0700

    Bài 32

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Portfolio của Đỗ Bá Kiệt đã sẵn sàng!');
+    // Một câu chào nhỏ khi truy cập
+    setTimeout(() => {
+        alert('Chào mừng bạn đến với không gian của Đỗ Bá Kiệt - Future Network Engineer!');
+    }, 500);
+});
